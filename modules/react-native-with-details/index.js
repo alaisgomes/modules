67ebdee2e6cbc7a963440e2dc0b1ac7391e78fab
alaisgomes <alaisgomes@gmail.com>
2024-04-02 17:29:56 -0400

    Adding new module with more fields

---
+++ modules/react-native-with-details/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function WithDetails() {
+  return (
+    <View>
+      <Text>with-details</Text>
+    </View>
+  );
+}
+
+export default {
+  title: "with-details",
+  navigator: WithDetails
+};
